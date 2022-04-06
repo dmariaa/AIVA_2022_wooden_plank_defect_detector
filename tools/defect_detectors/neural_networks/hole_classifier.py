c5a8ec3a04d1a706544215721ccab3975b76d677
David María Arribas <dmariaa70@gmail.com>
2022-04-06 10:05:09 +0200

    New: initial commit of hole classifier

---
+++ tools/defect_detectors/neural_networks/hole_classifier.py
@@ -0,0 +1,115 @@
+import os.path
+import pickle
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+from sklearn.model_selection import train_test_split
+
+
+def create_model(input_shape: tuple):
+    # create the common input layer
+    input_layer = layers.Input(input_shape)
+
+    # create the base layers
+    base_layers = layers.Conv2D(16, 3, padding='same', activation='relu', name='bl_2')(input_layer)
+    base_layers = layers.MaxPooling2D(name='bl_3')(base_layers)
+    base_layers = layers.Conv2D(32, 3, padding='same', activation='relu', name='bl_4')(base_layers)
+    base_layers = layers.MaxPooling2D(name='bl_5')(base_layers)
+    base_layers = layers.Conv2D(64, 3, padding='same', activation='relu', name='bl_6')(base_layers)
+    base_layers = layers.MaxPooling2D(name='bl_7')(base_layers)
+    base_layers = layers.Flatten(name='bl_8')(base_layers)
+
+    # create the localiser branch
+    locator_branch = layers.Dense(128, activation='relu', name='bb_1')(base_layers)
+    locator_branch = layers.Dense(64, activation='relu', name='bb_2')(locator_branch)
+    locator_branch = layers.Dense(32, activation='relu', name='bb_3')(locator_branch)
+    locator_branch = layers.Dense(4, activation='sigmoid', name='bb_head')(locator_branch)
+
+    model = Model(input_layer, outputs=[locator_branch])
+
+    return model
+
+
+def prepare_data(images: list, bboxes: list):
+    # image is in BGR format, normalize to 0.-1.
+    images = np.array(images)
+    images = images / 255.
+
+    # extract bboxes
+    single_bboxes = np.zeros((images.shape[0], 4))
+    for i, bbox in enumerate(bboxes):
+        if bbox.shape[0] > 0:
+            b = bbox[0]
+
+            image = images[i]
+            image_width, image_height, _ = image.shape
+
+            # transform (x1, y1, x2, y2) to (x, y, w, h)
+            b[2] = b[2] - b[0]
+            b[3] = b[3] - b[0]
+
+            # normalize b to 0-1 range
+            b[0] = b[0] / image_width
+            b[2] = b[2] / image_width
+            b[1] = b[1] / image_width
+            b[3] = b[3] / image_width
+
+            single_bboxes[i] = b
+
+    X_train, X_val, Y_train, Y_val = train_test_split(images, single_bboxes, test_size=0.2, random_state=8)
+    return X_train, X_val, Y_train, Y_val
+
+
+def load_pickle_file(pickle_file):
+    with open(pickle_file, 'rb') as pickle_file:
+        data = pickle.load(pickle_file)
+        pickle_file.close()
+
+    images = data['images']
+    image_bounding_boxes = data['bboxes']
+    bboxes = []
+
+    for ibboxes in image_bounding_boxes:
+        bboxes.append(ibboxes.to_xyxy_array())
+
+    return images, bboxes
+
+
+def train(data: str, lr: float = 1e-4, batch_size: int = 5, epochs: int = 100):
+    if os.path.isfile(data):
+        images, bboxes = load_pickle_file(data)
+
+    X_train, X_val, Y_train, Y_val = prepare_data(images, bboxes)
+
+    model = create_model(X_train.shape[1:])
+    loss = tf.keras.losses.MSE
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+
+    model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+
+    model.fit(X_train, Y_train,
+              validation_data=(X_val, Y_val),
+              batch_size=batch_size,
+              epochs=epochs,
+              shuffle=True,
+              verbose=1)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    def options():
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument("-d", "--data",
+                            help="Dataset folder or pickle file",
+                            required=True,
+                            type=str)
+        return parser
+
+    args = options().parse_args()
+    data = args.data
+
+    model = create_model((1000, 100, 3))
+    train(data)
