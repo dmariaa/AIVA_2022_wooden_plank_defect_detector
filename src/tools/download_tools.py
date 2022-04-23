8bc0312c8d5715d941edc5edd87b93af3eada35a
David María Arribas <dmariaa70@gmail.com>
2022-04-23 13:42:17 +0200

    Fix: models folder is created if doesn't exist

--- src/tools/download_tools.py
+++ src/tools/download_tools.py
@@ -1,10 +1,17 @@
 import base64
+import os.path
+
 import urllib3
 import shutil
 import gdown
 
 
 def google_drive_download(link: str, destinaton_file: str):
+    # Make sure the base directory for destination file exists
+    base_dir = os.path.dirname(destinaton_file)
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
+
     gdown.download(link, destinaton_file, quiet=False)
 
 
